8fed78e3507b28dac57cba0005d9d447e4ee4d8a
Kyle Henriksen <khenriksenbootcamp@gmail.com>
2022-03-22 15:18:36 -0700

    uploading my files for project 2

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,7 +6,7 @@ const seedDatabase = async () => {
   const rosterData = await rosterData.bulkCreate(rosterData);
 
   // TODO: SEED DATA WHEN NPM RUN SEED IS CALLED IN TERMINAL
-
+  test
   process.exit(0);
 };
 
