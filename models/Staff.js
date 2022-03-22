1208dbeec209c712515f6272f4d66383096fc444
Brad Hodge <bhodge166@gmail.com>
2022-03-22 16:12:16 -0600

    updated models

---
+++ models/Staff.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Staff extends Model {}
+
+Staff.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    nationality: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+      },
+    },
+
+    occupation: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+      },
+    },
+    former_clubs: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isAlpha: true,
+      },
+    },
+    staff_id: {
+      type: DataTypes.Integer,
+      references: {
+        model: "roster",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "staff",
+  }
+);
+
+module.exports = Staff;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const User = require("./User");
 const Roster = require("./Roster");
+const Player = require("./Player");
+const Staff = require("./Staff");
 
-module.exports = { User, Roster };
+module.exports = { User, Roster, Player, Staff };
