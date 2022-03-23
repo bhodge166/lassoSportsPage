37d56ecb7060c8654d999ea9cedd4d8216b9cebc
Brad Hodge <bhodge166@gmail.com>
2022-03-23 10:22:47 -0600

    updated seed and model data to match

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,19 +1,17 @@
-const sequelize = require('../config/connection');
-const seedPlayers = require('./playerData.js');
-const seedStaff = require('./staffData');
-
+const sequelize = require("../config/connection");
+const seedPlayers = require("./playerData.js");
+const seedRoster = require("./rosterData");
+const seedStaff = require("./staffData");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-
-  await seedPlayer();
+  await seedRoster();
+  await seedPlayers();
 
   await seedStaff();
 
-
-  
   process.exit(0);
 };
-  
+
 seedDatabase();
