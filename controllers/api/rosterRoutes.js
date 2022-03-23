ce25317d83ac4ec8c954f1c9ed1ad8f12703cd9b
Peter Riggs <peter92riggs@gmail.com>
2022-03-23 11:18:03 -0600

    roster routes

--- controllers/api/rosterRoutes.js
+++ controllers/api/rosterRoutes.js
@@ -1,2 +1,39 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { Roster, Player, Staff} = require("../../models");
+
+// find all roster route
+router.get('/', async (req, res) => {
+    
+    try {
+      const rosterData = await Player.findAll({
+        include: {
+          model: Roster,
+        },
+      });
+      res.status(200).json(rosterData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+  // find one player by its `id` value
+router.get('/:id', async (req, res) => {
+    try {
+      const rosterData = await Player.findByPk(req.params.id, {
+        include: {
+          model: Roster,
+        },
+      });
+  
+      if (!rosterData) {
+        res.status(404).json({ message: 'No player found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(rosterData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+module.exports = router;
