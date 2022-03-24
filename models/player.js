c1e01a54a29471535d76984024af64948c6d6f65
Brad Hodge <bhodge166@gmail.com>
2022-03-24 11:55:57 -0600

    updated models to allow null. set default values

--- models/player.js
+++ models/player.js
@@ -13,22 +13,33 @@ Player.init(
     first_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        isAlpha: true,
+      },
     },
     last_name: {
       type: DataTypes.STRING,
       allowNull: false,
+      validate: {
+        isAlpha: true,
+      },
     },
     national_team: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "eng.png",
     },
     position: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "CM",
     },
     kit_number: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      validate: {
+        isNumeric: true,
+      },
     },
     date_of_birth: {
       type: DataTypes.STRING,
@@ -36,47 +47,85 @@ Player.init(
     },
     former_clubs: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "n/a",
     },
     games_played: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     starts: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     goals: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     shots_on_target: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     assists: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     passes_success_rate: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     yellow_cards: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     red_cards: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     tackle_success_rate: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+      },
     },
     file_name: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
+      defaultValue: "silhouette.png",
     },
   },
   {
