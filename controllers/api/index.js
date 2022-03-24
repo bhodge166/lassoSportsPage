f5cb96321b7a345af3bc9c031fc6368e47b45ef2
Brad Hodge <bhodge166@gmail.com>
2022-03-24 11:04:02 -0600

    updated routes and images

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const playerRoutes = require("./playerRoutes");
 const userRoutes = require("./userRoutes");
 const staffRoutes = require("./staffRoutes");
 
-router.use("/player", playerRoutes);
 router.use("/user", userRoutes);
 router.use("/staff", staffRoutes);
 
